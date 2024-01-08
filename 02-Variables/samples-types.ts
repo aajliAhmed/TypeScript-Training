f0c3fd99a63674d2af5b34f80918cedec7b23fe8
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-01-08 16:54:57 +0100

    template string

--- 02-Variables/samples-types.ts
+++ 02-Variables/samples-types.ts
@@ -6,3 +6,4 @@ let lastName : string = "Ahmed";
 console.log(found);
 console.log("the number is "+ grade);
 console.log("Hi"+ firstName + " "+lastName);
+console.log(`HI ${firstName} ${lastName}`);
