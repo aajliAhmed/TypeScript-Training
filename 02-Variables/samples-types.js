1f0c4af8821664bdb7a9d4e22b2f27612ffbd3b7
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-01-08 16:49:39 +0100

    add some variables

---
+++ 02-Variables/samples-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 26;
+var firstName = "AAJLI";
+var lastName = "Ahmed";
+console.log(found);
+console.log("the number is " + grade);
+console.log("Hi" + firstName + " " + lastName);
---
+++ 02-Variables/samples-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade : number = 26;
+let firstName : string = "AAJLI";
+let lastName : string = "Ahmed";
+
+console.log(found);
+console.log("the number is "+ grade);
+console.log("Hi"+ firstName + " "+lastName);
