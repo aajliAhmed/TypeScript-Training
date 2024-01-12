0ed17ba80319c86674abc6dbadd6d3e45e65505e
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-01-12 17:36:31 +0100

    add array loop simple

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -2,3 +2,7 @@ var sports = ["Golf", "Cricket", "Tennis", "Swimming"];
 for (var i = 0; i < sports.length; i++) {
     console.log(sports[i]);
 }
+for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
+    var sportsOne = sports_1[_i];
+    console.log(sportsOne);
+}
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -3,3 +3,7 @@ let sports : string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
 for(let i=0;i<sports.length; i++){
     console.log(sports[i]);
 }
+
+for (let sportsOne of sports){
+    console.log(sportsOne);
+}
