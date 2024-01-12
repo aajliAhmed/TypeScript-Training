d51754fa5daa534679737b7b702112251f1d3b62
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-01-12 17:29:22 +0100

    add average with loops

--- 03-loops-and-arrays/reviews.js
+++ 03-loops-and-arrays/reviews.js
@@ -1,4 +1,8 @@
 var reviews = [5, 5, 4.5, 3, 2];
+var total = 0;
 for (var i = 0; i < reviews.length; i++) {
     console.log(reviews[i]);
+    total += reviews[i];
 }
+var average = total / reviews.length;
+console.log("the avrage is " + average);
