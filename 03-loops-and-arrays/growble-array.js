f1eb35954f91341a4fb7ee9dc7b7c086c70aefa4
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-01-15 17:45:01 +0100

    add growble array with push

---
+++ 03-loops-and-arrays/growble-array.js
@@ -0,0 +1,7 @@
+var sportsTwo = ["Tennis", "Cricket", "Swimming"];
+sportsTwo.push("futball");
+sportsTwo.push("fitness");
+for (var _i = 0, sportsTwo_1 = sportsTwo; _i < sportsTwo_1.length; _i++) {
+    var sportsO = sportsTwo_1[_i];
+    console.log(sportsO);
+}
---
+++ 03-loops-and-arrays/growble-array.ts
@@ -0,0 +1,7 @@
+let sportsTwo : string[] = ["Tennis","Cricket","Swimming"];
+
+sportsTwo.push("futball");
+sportsTwo.push("fitness");
+for(let sportsO of sportsTwo){
+    console.log(sportsO);
+}
