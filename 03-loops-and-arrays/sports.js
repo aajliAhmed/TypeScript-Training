ebc13a0bf289e2cb95cad2cda3e7f4017a54d2ea
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-01-15 17:40:27 +0100

    add condition in arrays

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -1,8 +1,15 @@
 var sports = ["Golf", "Cricket", "Tennis", "Swimming"];
-for (var i = 0; i < sports.length; i++) {
+/*
+for(let i=0;i<sports.length; i++){
     console.log(sports[i]);
 }
+*/
 for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
     var sportsOne = sports_1[_i];
-    console.log(sportsOne);
+    if (sportsOne == "Cricket") {
+        console.log(sportsOne + "  favorite sports");
+    }
+    else {
+        console.log(sportsOne);
+    }
 }
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -1,9 +1,15 @@
 let sports : string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
-
+/*
 for(let i=0;i<sports.length; i++){
     console.log(sports[i]);
 }
-
+*/
 for (let sportsOne of sports){
-    console.log(sportsOne);
+    if(sportsOne == "Cricket"){
+        console.log(sportsOne +"  favorite sports")
+    }
+    else{
+        console.log(sportsOne);
+    }
+    
 }
