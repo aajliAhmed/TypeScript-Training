923bcfd8ff4b6608c839380acba01e1b547518b6
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2024-12-21 11:10:20 +0100

    add methode

--- 05-inheritence/Shape.ts
+++ 05-inheritence/Shape.ts
@@ -1,5 +1,9 @@
 export class Shape {
-    private _x: number;
+    
+    
+    constructor(private _x: number,private _y: number){
+
+    };
 
     public get x(): number {
         return this._x;
@@ -8,14 +12,16 @@ export class Shape {
         this._x = value;
     }
 
-    private _y: number;
-    
     public get y(): number {
         return this._y;
     }
     public set y(value: number) {
         this._y = value;
     }
+
+   getInfo(): string{
+    return `x=${this._x},y=${this._y}`;
+   }
 
 
 }
